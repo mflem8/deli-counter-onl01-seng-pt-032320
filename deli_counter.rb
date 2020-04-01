aa22ce7e9c0a1be766c0003e8f295dd26b7ae542
Matt Fleming <matthewfleming8@gmail.com>
2020-04-01 00:57:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,43 +1,30 @@
+
 katz_deli = []
-#line
+
 def line(katz_deli)
-  phrase = "The line is currently: "
-  if katz_deli.length > 0 
-    katz_deli.each_with_index do |name, index|
-      phrase += "#{index + 1}. #{name}"
-    end
-    puts phrase
-  else
+  line_positions = [];
+  if katz_deli.empty?
     puts "The line is currently empty."
+  else
+    number = 1
+    katz_deli.each do |customer|
+      line_positions << "#{number}. #{customer}"
+      number += 1
+    end
+    puts "The line is currently: #{line_positions.join(" ")}"
   end
 end
 
-#take a number
 def take_a_number(katz_deli, name)
-	katz_deli.push(name)
-	puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-#	end
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
-
-#now serving
 def now_serving(katz_deli)
-  if katz_deli == 0
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
-  else 
-	 puts "Currently serving " + katz_deli[0] +"."
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
   end
-  katz_deli.shift
 end
-
-
-take_a_number(katz_deli, "Igor")
-take_a_number(katz_deli, "Bob")
-take_a_number(katz_deli, "Melanie")
- line(katz_deli)
- now_serving(katz_deli)
- line(katz_deli)
- take_a_number(katz_deli, "Corgi")
- line(katz_deli)
- now_serving(katz_deli)
- line(katz_deli)
